95a00e629f544b4599193cce9fba58019d67b8a1
Austin Mazur <mazuraustin1@gmail.com>
2023-03-09 21:11:41 +0100

    File succesfully linked, working on getting previous code working with the player class

--- classes/player.js
+++ classes/player.js
@@ -1,25 +1,133 @@
 class Player {
-    constructor(ctx, image) {
+    constructor(ctx, canvas) {
         this.x = 10;
-        this.y = 600;
         this.width = 50;
         this.height = 50;
+        this.y = canvas.height - this.height;
         this.ctx = ctx;
-        this.img = new Image();
-        this.img.src = image;
-        this.grav = 0; //"TODO" look up how to make gravity effect
+        // this.draw = this.draw.bind(this)
+        //this.img = new Image();
+        //this.img.src = image;
+        this.x_velocity = 0;
+        this.y_velocity = 0;
+        this.jumping = false;
+        this.left = false;
+        this.right = false;
+        this.up = false
+        this.type = "keydown"
+        this.keyListener = function arrow(event) {
+            const keyState = event.type === "keydown" ? true : false;
+          
+            switch (event.key) {
+              case "ArrowLeft": // left key
+                this.left = keyState;
+                break;
+              case "ArrowUp": // up key
+                this.up = keyState;
+                break;
+              case "ArrowRight": // right key
+                this.right = keyState;
+                break;
+            }
+          }.bind(this); //bind 'this' to the function
     }
 
     draw() {
-        // this.ctx.drawImage(this.x, this.y, this.width, this.height);
-        // this.ctx.fillStyle = "black"
-        // this.ctx.fillRect(this.x, this.y, this.width, this.height)
-        this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+         this.ctx.fillStyle = "black"
+         this.ctx.fillRect(this.x, this.y, this.width, this.height)
+       // this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
-    moveUp() {
-
-    }
+    // arrow(event) {
+    //     const keyState = event.type === "keydown" ? true : false;
+      
+    //     switch (event.key) {
+    //       case "ArrowLeft": // left key
+    //         movement.left = keyState;
+    //         break;
+    //       case "ArrowUp": // up key
+    //         movement.up = keyState;
+    //         break;
+    //       case "ArrowRight": // right key
+    //         movement.right = keyState;
+    //         break;
+    //     }
+    //   }
 }
 
-// export default Player;
+export default Player;
+
+
+
+
+
+
+
+
+// function (event) {
+//     const keyState = event.type === "keydown" ? true : false;
+
+//     switch (event.key) {
+//       case "ArrowLeft": // left key
+//         movement.left = keyState;
+//         break;
+//       case "ArrowUp": // up key
+//         movement.up = keyState;
+//         break;
+//       case "ArrowRight": // right key
+//         movement.right = keyState;
+//         break;
+//     }
+//   },
+
+
+
+ // if (movement.up && rectangle.jumping == false) {
+  //   //controls jump height
+  //   rectangle.y_velocity -= 30;
+  //   rectangle.jumping = true;
+  // }
+
+  // // controls the left speed
+  // if (movement.left) {
+  //   rectangle.x_velocity -= 0.8;
+  // }
+  // controls the right speed
+  // if (movement.right) {
+  //   rectangle.x_velocity += 0.8;
+  // }
+
+//   document.addEventListener("keyup", (e) => {
+//     delete keys[e.key];
+//     if (e.key === "ArrowUp") {
+//       isJumping = false;
+//     }
+//     if (e.key === "ArrowLeft") {
+//       movement.left = false;
+//       rectangle.x_velocity = 0;
+//     }
+//     if (e.key === "ArrowRight") {
+//       movement.right = false;
+//       rectangle.x_velocity = 0;
+//     }
+//   });
+
+
+
+// if (e.key === "ArrowUp" && !isJumping) {
+//     isJumping = true;
+//     movement.up = true;
+//     rectangle.y_velocity -= 30;
+//     rectangle.jumping = false;
+//     // console.log("up");
+//   }
+
+//   if (e.key === "ArrowLeft") {
+//     movement.left = true;
+//     rectangle.x_velocity -= 0.8;
+//   }
+
+//   if (e.key === "ArrowRight") {
+//     movement.right = true;
+//     rectangle.x_velocity += 0.8;
+//   }
--- script.js
+++ script.js
@@ -1,92 +1,153 @@
-// import Player from "./classes/player.js";
+import Player from "./classes/player.js";
 
 const canvas = document.querySelector("#canvas");
 const ctx = document.querySelector("canvas").getContext("2d");
 
+const myPlayer = new Player(ctx, canvas);
 
-const rectangle = {
-  height: 32,
-  jumping: true,
-  width: 32,
-  x: 10, 
-  x_velocity: 0,
-  y: 0,
-  y_velocity: 0,
-};
+//listening for keydown & the keylistener function in the player class
+document.addEventListener(myPlayer.type, myPlayer.keyListener);
 
-const movement = {
-  left: false,
-  right: false,
-  up: false,
-  keyListener: function (event) {
-    const keyState = event.type === "keydown" ? true : false;
+// const rectangle = {
+//   height: 32,
+//   jumping: true,
+//   width: 32,
+//   x: 10, 
+//   x_velocity: 0,
+//   y: 0,
+//   y_velocity: 0,
+// };
 
-    switch (event.key) {
-      case "ArrowLeft": // left key
-        movement.left = keyState;
-        break;
-      case "ArrowUp": // up key
-        movement.up = keyState;
-        break;
-      case "ArrowRight": // right key
-        movement.right = keyState;
-        break;
-    }
-  },
-};
+// myPlayer.draw()
 
-const loop = function () {
-  if (movement.up && rectangle.jumping == false) {
+// const movement = {
+//   left: false,
+//   right: false,
+//   up: false,
+//   keyListener: arrow
+// };
+
+
+
+function gameLoop() {
+  //Updating players postion
+  // if(myPlayer.left) {
+  //   myPlayer.x_velocity -= 0.8;
+  // }
+  // if(myPlayer.right) {
+  //   myPlayer.x_velocity += 0.8;
+  // }
+  // if(myPlayer.up && !myPlayer.jumping) {
+  //   myPlayer.y_velocity -= 30;
+  //   myPlayer.jumping = true;
+  // }
+
+  // myPlayer.x += myPlayer.x_velocity;
+  // myPlayer.y += myPlayer.y_velocity;
+  // myPlayer.x_velocity *= 0.9;
+  // myPlayer.y_velocity *= 0.9;
+
+  //background
+  ctx.fillStyle = "grey";
+  ctx.fillRect(0, 0, 800, 600);
+  //draw player
+  myPlayer.draw();
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillRect(myPlayer.x, myPlayer.y, myPlayer.width, myPlayer.height)
+  
+
+  if (myPlayer.up && myPlayer.jumping == false) {
     //controls jump height
-    rectangle.y_velocity -= 30; 
-    rectangle.jumping = true;
+    myPlayer.y_velocity -= 30; 
+    myPlayer.jumping = true;
   }
 
   // controls the left speed
-  if (movement.left) {
-    rectangle.x_velocity -= 0.8;
+  if (myPlayer.left) {
+    myPlayer.x_velocity -= 0.8;
   }
   // controls the right speed
-  if (movement.right) {
-    rectangle.x_velocity += 0.8;
+  if (myPlayer.right) {
+    myPlayer.x_velocity += 0.8;
   }
 
-  rectangle.y_velocity += 1.5; // gravity
-  rectangle.x += rectangle.x_velocity;
-  rectangle.y += rectangle.y_velocity;
-  rectangle.x_velocity *= 0.9; // friction illusion
-  rectangle.y_velocity *= 0.96; // friction illusion
+  myPlayer.y_velocity += 1.5; // gravity
+  myPlayer.x += myPlayer.x_velocity;
+  myPlayer.y += myPlayer.y_velocity;
+  myPlayer.x_velocity *= 0.9; // friction illusion
+  myPlayer.y_velocity *= 0.96; // friction illusion
 
   // keeps obj from falling past canvas
-  if (rectangle.y > 565) {
-    rectangle.jumping = false;
-    rectangle.y = 565;
-    rectangle.y_velocity = 0;
+  if (myPlayer.y > 565) {
+    myPlayer.jumping = false;
+    myPlayer.y = 565;
+    myPlayer.y_velocity = 0;
   }
 
   // creating left and right border
-  if (rectangle.x < 0) {
-    rectangle.x = 0;
-  } else if (rectangle.x > 765) {
-    rectangle.x = 765;
+  if (myPlayer.x < 0) {
+    myPlayer.x = 0;
+  } else if (myPlayer.x > 765) {
+    myPlayer.x = 765;
   }
+  
 
 
   // creating the square
-  //the black background
-  ctx.fillStyle = "#202020";
-  ctx.fillRect(0, 0, 800, 600);
+  //background
+  
 
-   // red square
-  ctx.fillStyle = "#ff0000";
-  ctx.beginPath();
-  ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-  ctx.fill();
+   // little square
+  // ctx.fillStyle = "cyan";
+  // ctx.beginPath();
+  // // ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+  // ctx.fill();
+  
+  
+  
 
   // call update when the browser is ready to draw again
-  window.requestAnimationFrame(loop);
+  window.requestAnimationFrame(gameLoop);
 };
 
-window.addEventListener("keydown", movement.keyListener);
-window.addEventListener("keyup", movement.keyListener);
-window.requestAnimationFrame(loop);
+// window.addEventListener("keydown", movement.keyListener);
+// window.addEventListener("keyup", movement.keyListener);
+window.requestAnimationFrame(gameLoop);
+
+// gameLoop();
+
+
+// function arrow(event) {
+//   const keyState = event.type === "keydown" ? true : false;
+
+//   switch (event.key) {
+//     case "ArrowLeft": // left key
+//       movement.left = keyState;
+//       break;
+//     case "ArrowUp": // up key
+//       movement.up = keyState;
+//       break;
+//     case "ArrowRight": // right key
+//       movement.right = keyState;
+//       break;
+//   }
+// }
+
+
+// function arrow(event) {
+//   const keyState = event.type === "keydown" ? true : false;
+
+//   switch (event.key) {
+//     case "ArrowLeft": // left key
+//       myPlayer.left = keyState;
+//       break;
+//     case "ArrowUp": // up key
+//       movement.up = keyState;
+//       break;
+//     case "ArrowRight": // right key
+//       movement.right = keyState;
+//       break;
+//   }
+// }
+
+
